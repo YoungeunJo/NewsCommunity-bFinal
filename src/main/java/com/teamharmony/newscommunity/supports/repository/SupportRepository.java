ab3ab6020c540213aef1870805e8d741d9f89043
YoungeunJo <youngeun.jo1494@gmail.com>
2022-07-13 17:06:56 +0900

    Feat: 고객 문의글 생성시 email 수집 #56

--- src/main/java/com/teamharmony/newscommunity/supports/repository/SupportRepository.java
+++ src/main/java/com/teamharmony/newscommunity/supports/repository/SupportRepository.java
@@ -1,9 +1,7 @@
 package com.teamharmony.newscommunity.supports.repository;
 
-
 import com.teamharmony.newscommunity.supports.entity.Support;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface SupportRepository extends JpaRepository<Support, Long> {
